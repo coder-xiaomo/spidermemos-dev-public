e8cbfeafd8514ca767d23523e64217c6542bb020
coder-xiaomo <16949192+coder-xiaomo@users.noreply.github.com>
2022-12-01 09:40:40 +0000

    Update main to output generated at 20b0d9e

---
+++ assets/User.01404be8.js
@@ -0,0 +1,1 @@
+import{_ as o,j as t,o as s,c,g as n,e as r}from"./index.784c47c5.js";const a={},_=r("div",null," User ",-1);function l(d,i){const e=t("RouterView");return s(),c("div",null,[_,n(e)])}const f=o(a,[["render",l]]);export{f as default};
