b6c1005a07f75ff9e2b289dc36b27bf39798b6d3
coder-xiaomo <16949192+coder-xiaomo@users.noreply.github.com>
2026-06-13 11:13:18 +0000

    Update main to output generated at b984f32

---
+++ _nuxt/BVpkCGGX.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,a as n,o as t}from"./-sGV08ty.js";const a={};function r(_,e){return t(),s("div",null,[...e[0]||(e[0]=[n("span",null,"404 Not Found",-1),n("span",null,"啊偶 页面消失啦！",-1)])])}const c=o(a,[["render",r]]);export{c as default};
